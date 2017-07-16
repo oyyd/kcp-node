f55e38dff5d5cd5f66df113c8b0cb69b27484f60
oyyd <oyydoibh@gmail.com>
2017-07-16 22:53:03 +0800

    update

--- src/__tests__/recv.spec.js
+++ src/__tests__/recv.spec.js
@@ -1,4 +1,4 @@
-import { getPeeksize } from '../recv'
+import { moveRcvBuf, popRcvContent, getPeeksize } from '../recv'
 import { createTestKCP } from './utils'
 
 describe('recv.js', () => {
@@ -45,6 +45,146 @@ describe('recv.js', () => {
       kcp.nrcv_que = kcp.rcv_queue.length
 
       expect(getPeeksize(kcp)).toBe(1000)
+    })
+  })
+
+  describe('popRcvContent', () => {
+    it('should pop buffer from the `rcv_queue` ', () => {
+      kcp.rcv_queue = [{
+        len: 2,
+        data: Buffer.from('FFFF', 'hex'),
+        frg: 2,
+      }, {
+        len: 2,
+        data: Buffer.from('EEEE', 'hex'),
+        frg: 1,
+      }, {
+        len: 2,
+        data: Buffer.from('DDDD', 'hex'),
+        frg: 0,
+      }, {
+        len: 2,
+        data: Buffer.from('CCCC', 'hex'),
+        frg: 0,
+      }]
+
+      kcp.nrcv_que = kcp.rcv_queue.length
+      const buffer = popRcvContent(kcp)
+
+      expect(buffer.length).toBe(6)
+      expect(buffer.toString('hex')).toBe('ffffeeeedddd')
+      expect(kcp.rcv_queue.length).toBe(1)
+    })
+  })
+
+  describe('moveRcvBuf', () => {
+    it('should move those buf whose sn is smaller than ' +
+      'the `rcv_nxt` while `nrcv_que` is smaller than the `rcv_wnd`', () => {
+      kcp.rcv_buf = [{
+        len: 2,
+        data: Buffer.from('FFFF', 'hex'),
+        frg: 2,
+        sn: 0,
+      }, {
+        len: 2,
+        data: Buffer.from('EEEE', 'hex'),
+        frg: 1,
+        sn: 1,
+      }, {
+        len: 2,
+        data: Buffer.from('DDDD', 'hex'),
+        frg: 0,
+        sn: 2,
+      }, {
+        len: 2,
+        data: Buffer.from('CCCC', 'hex'),
+        frg: 0,
+        sn: 3,
+      }]
+
+      kcp.rcv_nxt = 0
+      kcp.nrcv_buf = kcp.rcv_buf.length
+      kcp.rcv_queue = []
+      kcp.nrcv_que = 0
+      kcp.rcv_wnd = 3
+
+      moveRcvBuf(kcp)
+
+      expect(kcp.rcv_queue.map(seg => seg.sn)).toEqual([0, 1, 2])
+      expect(kcp.rcv_buf.map(seg => seg.sn)).toEqual([3])
+      expect(kcp.rcv_nxt).toBe(3)
+    })
+
+    it('should respect the rcv_nxt', () => {
+      kcp.rcv_buf = [{
+        len: 2,
+        data: Buffer.from('FFFF', 'hex'),
+        frg: 2,
+        sn: 0,
+      }, {
+        len: 2,
+        data: Buffer.from('EEEE', 'hex'),
+        frg: 1,
+        sn: 1,
+      }, {
+        len: 2,
+        data: Buffer.from('DDDD', 'hex'),
+        frg: 0,
+        sn: 2,
+      }, {
+        len: 2,
+        data: Buffer.from('CCCC', 'hex'),
+        frg: 0,
+        sn: 3,
+      }]
+
+      kcp.rcv_nxt = 1
+      kcp.nrcv_buf = kcp.rcv_buf.length
+      kcp.rcv_queue = []
+      kcp.nrcv_que = 0
+      kcp.rcv_wnd = 3
+
+      moveRcvBuf(kcp)
+
+      expect(kcp.rcv_queue.map(seg => seg.sn)).toEqual([])
+      expect(kcp.rcv_buf.map(seg => seg.sn)).toEqual([0, 1, 2, 3])
+      expect(kcp.rcv_nxt).toBe(1)
+    })
+
+    it('should respect the `rcv_wnd`', () => {
+      kcp.rcv_buf = [{
+        len: 2,
+        data: Buffer.from('FFFF', 'hex'),
+        frg: 2,
+        sn: 0,
+      }, {
+        len: 2,
+        data: Buffer.from('EEEE', 'hex'),
+        frg: 1,
+        sn: 1,
+      }, {
+        len: 2,
+        data: Buffer.from('DDDD', 'hex'),
+        frg: 0,
+        sn: 2,
+      }, {
+        len: 2,
+        data: Buffer.from('CCCC', 'hex'),
+        frg: 0,
+        sn: 3,
+      }]
+
+      kcp.rcv_nxt = 0
+      kcp.nrcv_buf = kcp.rcv_buf.length
+      kcp.rcv_queue = []
+      kcp.nrcv_que = 0
+      kcp.rcv_wnd = 2
+
+      moveRcvBuf(kcp)
+
+      expect(kcp.rcv_queue.map(seg => seg.sn)).toEqual([0, 1])
+      expect(kcp.rcv_buf.map(seg => seg.sn)).toEqual([2, 3])
+      expect(kcp.rcv_nxt).toBe(2)
     })
   })
 })
--- src/recv.js
+++ src/recv.js
@@ -40,6 +40,41 @@ export function getPeeksize(kcp) {
   return length
 }
 
+// @private
+export function popRcvContent(kcp) {
+  let i = 0
+
+  for (; i < kcp.nrcv_que; i += 1) {
+    if (kcp.rcv_queue[i].frg === 0) {
+      break
+    }
+  }
+
+  const segSlice = kcp.rcv_queue.splice(0, i + 1)
+  kcp.nrcv_que -= i + 1
+  return Buffer.concat(segSlice.map(seg => seg.data))
+}
+
+// @private
+export function moveRcvBuf(kcp) {
+  let i
+  const rcvBufLength = kcp.rcv_buf.length
+
+  for (i = 0; i < rcvBufLength; i += 1) {
+    const seg = kcp.rcv_buf[i]
+
+    if (!(kcp.rcv_nxt + i === seg.sn && kcp.nrcv_que + i < kcp.rcv_wnd)) {
+      break
+    }
+  }
+
+  const rcvBufSlice = kcp.rcv_buf.splice(0, i)
+  kcp.nrcv_buf -= i
+  kcp.rcv_queue.push(...rcvBufSlice)
+  kcp.nrcv_que += i
+  kcp.rcv_nxt += i
+}
+
 // NOTE: the `buffer` is not a pointer
 // so we return a buffer directly
 export function recv(kcp) {
@@ -61,33 +96,9 @@ export function recv(kcp) {
     recover = 1
   }
 
-  let i = 0
+  const buffer = popRcvContent(kcp)
 
-  for (; i < kcp.nrcv_que; i += 1) {
-    if (kcp.rcv_queue[i].frg === 0) {
-      break
-    }
-  }
-
-  const segSlice = kcp.rcv_queue.splice(0, i + 1)
-  kcp.nrcv_que -= i + 1
-  const buffer = Buffer.concat(segSlice.map(seg => seg.data))
-
-  const rcvBufLength = kcp.rcv_buf.length
-
-  for (i = 0; i < rcvBufLength; i += 1) {
-    const seg = kcp.rcv_buf[i]
-
-    if (!(seg.sn === kcp.rcv_nxt && kcp.nrcv_que < kcp.rcv_wnd)) {
-      break
-    }
-  }
-
-  const rcvBufSlice = kcp.rcv_buf.splice(0, i)
-  kcp.nrcv_buf -= i
-  kcp.rcv_queue.push(...rcvBufSlice)
-  kcp.nrcv_que += i
-  kcp.rcv_nxt += i
+  moveRcvBuf(kcp)
 
   // fast recover
   if (kcp.nrcv_que < kcp.rcv_wnd && recover) {
